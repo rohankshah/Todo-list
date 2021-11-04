54f9ed1179f19e2c857272fbd1e426320233ddc9
rohankshah <moreinmore@gmail.com>
2021-11-04 19:33:00 +0530

    add function to show tasks on screen

--- src/mainTab.js
+++ src/mainTab.js
@@ -1,6 +1,7 @@
 import './mainTab.css';
 import { addFlex, createHeading } from './pageload';
 
+let index = 0;
 let list = [];
 let projectName = '';
 
@@ -10,7 +11,7 @@ function mainTabLoad(ev) {
     mainTab.innerHTML = '';
     mainTab.classList.add("mainTab")
     addFlex(mainTab);
-    createHeading(mainTab, `${projectName} task's`);
+    createHeading(mainTab, `${projectName} Task List`);
     createMainCont(mainTab);
     createMainInput(mainTab);
     updateList();
@@ -27,10 +28,14 @@ function createMainInput(mainTab) {
     const mainInputDiv = document.createElement("div");
     mainInputDiv.classList.add("mainInputDiv");
     const mainTask = document.createElement("input");
+    mainTask.classList.add("mainTask");
+    mainTask.placeholder = "Enter task details"
     const mainDate = document.createElement("input");
-    mainDate.type = "date"
+    mainDate.type = "date";
+    mainDate.classList.add("mainDate");
     const mainAdd = document.createElement("button");
     mainAdd.innerHTML = "Add task";
+    mainAdd.classList.add("mainAdd");
     mainInputDiv.appendChild(mainTask);
     mainInputDiv.appendChild(mainDate);
     mainInputDiv.appendChild(mainAdd);
@@ -45,16 +50,19 @@ function monitorMainAdd() {
 
 function addTask() {
     const mainInputDiv = document.getElementsByClassName("main")[0];
-    const mainTaskInput = mainInputDiv.children[2].children[0].value;
-    const mainTaskDate = mainInputDiv.children[2].children[1].value;
-    // console.log(mainTaskInput);
-    // console.log(mainTaskDate);
+    const mainTaskInput = mainInputDiv.children[2].children[0];
+    const mainTaskDate = mainInputDiv.children[2].children[1];
+
     let list_obj = {
-        task: mainTaskInput,
-        due_date: mainTaskDate
+        task: mainTaskInput.value,
+        due_date: mainTaskDate.value
     }
     list.push(list_obj);
     pushTask();
+    showTask();
+
+    mainTaskInput.value = '';
+    mainTaskDate.value = '';
 }
 
 function pushTask() {
@@ -62,11 +70,74 @@ function pushTask() {
     let parsed = JSON.parse(unparsed);
     for (let i = 0; i < parsed.length; i++) {
         if (parsed[i].name === projectName) {
+            index = i;
             parsed[i].list_objs = list;
             localStorage.setItem("project_list", JSON.stringify(parsed));
         }
     }
-    console.log(JSON.parse(localStorage.getItem("project_list")));
+}
+
+function showTask() {
+    const mainInputDiv = document.getElementsByClassName("main")[0].children[1];
+    mainInputDiv.innerHTML = '';
+    const unparsed = localStorage.getItem('project_list');
+    const parsed = JSON.parse(unparsed)[index];
+    const parsedList = parsed.list_objs;
+    for (let i = 0; i < parsedList.length; i++) {
+        let taskDiv = document.createElement("div");
+        taskDiv.classList.add("taskDiv");
+
+        let taskDetails = document.createElement("p");
+        taskDetails.innerHTML = parsedList[i].task;
+        taskDetails.classList.add("taskDetails");
+
+        let taskDate = document.createElement("button");
+        taskDate.innerHTML = "Due: " + parsedList[i].due_date;
+        taskDate.classList.add("taskDate")
+
+        let taskDelete = document.createElement("button");
+        taskDelete.classList.add("taskDelete");
+        taskDelete.innerHTML = "Delete";
+
+        taskDiv.appendChild(taskDetails);
+        taskDiv.appendChild(taskDate);
+        taskDiv.appendChild(taskDelete);
+        mainInputDiv.appendChild(taskDiv);
+    }
+    monitorDeleteBtn();
+}
+
+function monitorDeleteBtn() {
+    const mainInputDiv = document.getElementsByClassName("main")[0].children[1];
+    const tasks = mainInputDiv.children;
+    Array.from(tasks).forEach(ele => {
+        let taskDeletebtn = ele.children[2];
+        taskDeletebtn.addEventListener('click', taskDelete);
+    })
+}
+
+function taskDelete(ev) {
+    let taskDetail = ev.path[1].children[0].innerHTML;
+    let projectList = localStorage.getItem('project_list');
+    let parsedProjectList = JSON.parse(projectList);
+    let currentProject = parsedProjectList[index];
+    let currentProjectList = currentProject.list_objs;
+
+    // let unparsed = localStorage.getItem('project_list');
+    // let parsed = JSON.parse(unparsed)[index];
+    // let parsedList = parsed.list_objs;
+    // for (let i = 0; i < parsedList.length; i++) {
+    //     if(parsedList[i].task === taskDetail) {
+
+    //     }
+    // }
+
+    const filteredList = currentProjectList.filter(function(el) { return el.task != taskDetail; }); 
+    let a = JSON.parse(projectList)
+    a[index].list_objs = filteredList;
+    let newProjectList = JSON.stringify(a);
+    localStorage.setItem('project_list', newProjectList);
+    showTask();
 }
 
 function updateList() {
